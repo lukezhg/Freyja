95306502bce73fe55e2fa6d4b17729346140c6fb
lukezhg <lukezhg@gmail.com>
2013-02-07 16:11:23 +0800

    update imag

--- freyja.sh
+++ freyja.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
-
-fileName=`basename "$1" | sed 's/ /_/g'`
-newFile="/home/lukezhang/github/Freyja/$fileName"
-mv "$1" "$newFile"
-cd "/home/lukezhang/github/Freyja/"
-git add $fileName
-git commit -m "Upload picture $fileName"
-git push -u origin master
+#!/bin/sh
+fileName=`basename "$1" | sed 's/ /_/g'`
+git add .
+git commit -m "Upload picture $fileName"
+git push origin master
 echo "https://github.com/khotyn/Freyja/raw/master/$fileName"
