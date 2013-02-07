ed03826a601aaa4c14dc97708cd625ff0a2f63a8
lukezhg <lukezhg@gmail.com>
2013-02-07 15:34:14 +0800

    try to use ubuntu update image

--- freyja.sh
+++ freyja.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
-fileName=`basename "$1" | sed 's/ /_/g'`
-cd "g:/github/Freyja/"
-git add $fileName
-git commit -m "Upload picture $fileName"
-git push -u origin master
-echo "https://github.com/lukezhg/Freyja/raw/master/$fileName"
+#!/bin/sh
+
+fileName=`basename "$1" | sed 's/ /_/g'`
+newFile="/home/lukezhang/github/Freyja/$fileName"
+mv "$1" "$newFile"
+cd "/home/lukezhang/github/Freyja/"
+git add $fileName
+git commit -m "Upload picture $fileName"
+git push -u origin master
+echo "https://github.com/khotyn/Freyja/raw/master/$fileName"
