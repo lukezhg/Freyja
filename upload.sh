865c0c77354b8ceab70c14dc1b2c0d709c583da8
lukezhg <lukezhg@gmail.com>
2013-02-07 21:05:26 +0800

    upload img

--- upload.sh
+++ upload.sh
@@ -2,8 +2,9 @@
 fileName=`basename "$1" | sed 's/ /_/g'`
 newFile="/home/lukezhang/github/Freyja/$fileName"
 echo "newFile"
+mv "$1" "$newFile"
 cd "/home/lukezhang/github/Freyja/"
 git add .
 git commit -m "upload img"
 git push origin master
-echo "upload finished"
+echo "https://github.com/lukezhg/Freyja/raw/master/$fileName"
