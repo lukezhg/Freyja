48e0181e64f91f3065cb021e259573f92777fa7c
Chunlei Zhang <lukezhg@gmail.com>
2013-11-12 15:31:25 -0500

    upload img

--- upload.sh
+++ upload.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd "/home/lukezhang/github/Freyja/"
+cd "E:/github/Freyja/"
 git add --all
 git commit -m "upload img"
 git push origin master
 echo "https://github.com/lukezhg/Freyja/raw/master/"
 echo "upload finished"
+cd "E:/github/octopress/"
